79d138d1083ba730bd08acc003b16f756974c95d
theomolfessis <theo.molfessis@prange.fr>
2025-01-12 13:39:08 +0100

    pre process for augmented data

--- code/utils.py
+++ code/utils.py
@@ -22,7 +22,7 @@ from extract_feats import extract_feats, extract_numbers
 
 
 
-def preprocess_dataset(dataset, n_max_nodes, spectral_emb_dim):
+def preprocess_dataset(dataset, n_max_nodes, spectral_emb_dim,permute= False):
 
     data_lst = []
     if dataset == 'test':
@@ -48,7 +48,97 @@ def preprocess_dataset(dataset, n_max_nodes, spectral_emb_dim):
             torch.save(data_lst, filename)
             print(f'Dataset {filename} saved')
 
+    elif dataset == 'augment':
+        if permute: 
+            augmented_list = ['adding_edges','adding_nodes','adding_triangles','changing_labels','changing_labels_bis']
+        else: 
+            augmented_list = ['adding_edges','adding_nodes','adding_triangles']
+        for datatype in augmented_list:
+            filename = './data/dataset_'+datatype+'.pt'
+            graph_path = './data_augmented/'+datatype+'/graph'
+            desc_path = './data_augmented/'+datatype+'/description'
+            if os.path.isfile(filename):
+                data_lst = torch.load(filename)
+                print(f'Dataset {filename} loaded from file')
+            else:
+                # traverse through all the graphs of the folder
+                files = [f for f in os.listdir(graph_path)]
+                adjs = []
+                eigvals = []
+                eigvecs = []
+                n_nodes = []
+                max_eigval = 0
+                min_eigval = 0
+                for fileread in tqdm(files):
+                    tokens = fileread.split("/")
+                    idx = tokens[-1].find(".")
+                    filen = tokens[-1][:idx]
+                    extension = tokens[-1][idx+1:]
+                    fread = os.path.join(graph_path,fileread)
+                    fstats = os.path.join(desc_path,filen+".txt")
+                    #load dataset to networkx
+                    if extension=="graphml":
+                        G = nx.read_graphml(fread)
+                        # Convert node labels back to tuples since GraphML stores them as strings
+                        G = nx.convert_node_labels_to_integers(
+                            G, ordering="sorted"
+                        )
+                    else:
+                        G = nx.read_edgelist(fread)
+                    # use canonical order (BFS) to create adjacency matrix
+                    ### BFS & DFS from largest-degree node
 
+                    
+                    CGs = [G.subgraph(c) for c in nx.connected_components(G)]
+
+                    # rank connected componets from large to small size
+                    CGs = sorted(CGs, key=lambda x: x.number_of_nodes(), reverse=True)
+
+                    node_list_bfs = []
+                    for ii in range(len(CGs)):
+                        node_degree_list = [(n, d) for n, d in CGs[ii].degree()]
+                        degree_sequence = sorted(
+                        node_degree_list, key=lambda tt: tt[1], reverse=True)
+
+                        bfs_tree = nx.bfs_tree(CGs[ii], source=degree_sequence[0][0])
+                        node_list_bfs += list(bfs_tree.nodes())
+
+                    adj_bfs = nx.to_numpy_array(G, nodelist=node_list_bfs)
+
+                    adj = torch.from_numpy(adj_bfs).float()
+                    diags = np.sum(adj_bfs, axis=0)
+                    diags = np.squeeze(np.asarray(diags))
+                    D = sparse.diags(diags).toarray()
+                    L = D - adj_bfs
+                    with np.errstate(divide="ignore"):
+                        diags_sqrt = 1.0 / np.sqrt(diags)
+                    diags_sqrt[np.isinf(diags_sqrt)] = 0
+                    DH = sparse.diags(diags).toarray()
+                    L = np.linalg.multi_dot((DH, L, DH))
+                    L = torch.from_numpy(L).float()
+                    eigval, eigvecs = torch.linalg.eigh(L)
+                    eigval = torch.real(eigval)
+                    eigvecs = torch.real(eigvecs)
+                    idx = torch.argsort(eigval)
+                    eigvecs = eigvecs[:,idx]
+
+                    edge_index = torch.nonzero(adj).t()
+
+                    size_diff = n_max_nodes - G.number_of_nodes()
+                    x = torch.zeros(G.number_of_nodes(), spectral_emb_dim+1)
+                    x[:,0] = torch.mm(adj, torch.ones(G.number_of_nodes(), 1))[:,0]/(n_max_nodes-1)
+                    mn = min(G.number_of_nodes(),spectral_emb_dim)
+                    mn+=1
+                    x[:,1:mn] = eigvecs[:,:spectral_emb_dim]
+                    adj = F.pad(adj, [0, size_diff, 0, size_diff])
+                    adj = adj.unsqueeze(0)
+
+                    feats_stats = extract_feats(fstats)
+                    feats_stats = torch.FloatTensor(feats_stats).unsqueeze(0)
+
+                    data_lst.append(Data(x=x, edge_index=edge_index, A=adj, stats=feats_stats, filename = filen))
+                torch.save(data_lst, filename)
+                print(f'Dataset {filename} saved')
     else:
         filename = './data/dataset_'+dataset+'.pt'
         graph_path = './data/'+dataset+'/graph'
