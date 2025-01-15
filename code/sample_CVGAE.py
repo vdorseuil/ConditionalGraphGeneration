702f2ed317af72dff925890965b1ca117ada8ff0
valentin.dorseuil@gmail.com <valentin.dorseuil@gmail.com>
2025-01-15 18:32:16 +0100

    Addinhg the possibility to sample many points and generate on the best one.

--- code/sample_CVGAE.py
+++ code/sample_CVGAE.py
@@ -1,47 +1,30 @@
 from torch_geometric.loader import DataLoader
+from tqdm import tqdm
 import torch
 import csv
 import argparse
 import os
+import numpy as np
 
 from model.cvae import CVGAE
 from utils.data_processing import preprocess_dataset, construct_nx_from_adj, get_stats_mean_std
-from utils.eval import read_output, MAE
+from utils.eval import read_output, MAE, calculate_stats_graph
 
 ###############################################################################
 
 # Argument parser
 parser = argparse.ArgumentParser(description='NeuralGraphGenerator')
 
-# Batch size
-parser.add_argument('--batch-size', type=int, default=256, help="Batch size for training, controlling the number of samples per gradient update (default: 256)")
-
-# Number of epochs for the autoencoder training
 parser.add_argument('--epochs-autoencoder', type=int, default=200, help="Number of training epochs for the autoencoder (default: 200)")
-
-# Hidden dimension size for the encoder network
 parser.add_argument('--hidden-dim-encoder', type=int, default=256, help="Hidden dimension size for encoder layers (default: 64)")
-
-# Hidden dimension size for the decoder network
 parser.add_argument('--hidden-dim-decoder', type=int, default=256, help="Hidden dimension size for decoder layers (default: 256)")
-
-# Dimensionality of the latent space
 parser.add_argument('--latent-dim', type=int, default=2, help="Dimensionality of the latent space in the autoencoder (default: 32)")
-
-# Maximum number of nodes of graphs
 parser.add_argument('--n-max-nodes', type=int, default=50, help="Possible maximum number of nodes in graphs (default: 50)")
-
-# Number of layers in the encoder network
 parser.add_argument('--n-layers-encoder', type=int, default=3, help="Number of layers in the encoder network (default: 2)")
-
-# Number of layers in the decoder network
 parser.add_argument('--n-layers-decoder', type=int, default=3, help="Number of layers in the decoder network (default: 3)")
-
-# Dimensionality of spectral embeddings for graph structure representation
 parser.add_argument('--spectral-emb-dim', type=int, default=10, help="Dimensionality of spectral embeddings for representing graph structures (default: 10)")
-
-# Number of conditions used in conditional vector (number of properties)
 parser.add_argument('--n-condition', type=int, default=7, help="Number of distinct condition properties used in conditional vector (default: 7)")
+parser.add_argument('--n-sample', type=int, default=1, help="Number of points to sample in the VGAE latent space. The one with the best MAE is kept for the final generation. (default: 1)")
 
 args = parser.parse_args()
 
@@ -52,7 +35,7 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 trainset = preprocess_dataset("train", args.n_max_nodes, args.spectral_emb_dim)
 testset = preprocess_dataset("test", args.n_max_nodes, args.spectral_emb_dim)
-test_loader = DataLoader(testset, batch_size=args.batch_size, shuffle=False)
+test_loader = DataLoader(testset, batch_size=1, shuffle=False)
 
 stats_mean, stats_std = get_stats_mean_std(trainset)
 
@@ -69,28 +52,31 @@ with open("outputs/output_CVGAE.csv", "w", newline="") as csvfile:
     writer.writerow(["graph_id", "edge_list"])
 
 
-    for data in test_loader:
+    for data in tqdm(test_loader, desc="Processing graphs"):
         data = data.to(device)
         stats = data.stats
-        adj = cvgae.sample(stats)
-        graph_ids = data.filename
 
+        best_graph = None
+        best_mae = np.inf
+        for k in range(args.n_sample):
+            adj = cvgae.sample(stats)
+            graph_ids = data.filename
 
-        for i in range(stats.size(0)):
-            stat_x = stats[i]
+            stat_x = stats[0] #batch size of 1
+            Gs_generated = construct_nx_from_adj(adj[0,:,:].detach().cpu().numpy())
+            test_stats = stat_x.detach().cpu().numpy()
+            pred_stats = calculate_stats_graph(Gs_generated)
+            mae =  np.mean(np.abs(pred_stats - test_stats))
 
-            Gs_generated = construct_nx_from_adj(adj[i,:,:].detach().cpu().numpy())
-            stat_x = stat_x.detach().cpu().numpy()
+            if mae < best_mae:
+                best_mae = mae
+                best_graph = Gs_generated
 
-            # Define a graph ID
-            graph_id = graph_ids[i]
-
-            # Convert the edge list to a single string
-            edge_list_text = ", ".join([f"({u}, {v})" for u, v in Gs_generated.edges()])           
-            # Write the graph ID and the full edge list as a single row
-            writer.writerow([graph_id, edge_list_text])
-
+        # Define a graph ID
+        graph_id = graph_ids[0]
+        edge_list_text = ", ".join([f"({u}, {v})" for u, v in best_graph.edges()])           
+        writer.writerow([graph_id, edge_list_text])
 
 Gs = read_output("outputs/output_CVGAE.csv")
-mae = MAE(Gs, testset)
-print(f"Mean absolute error of graph features: {mae:.2f}")
+mae_total = MAE(Gs, testset)
+print(f"Mean absolute error of graph features: {mae_total:.2f}")
